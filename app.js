ed3b8adf95bc2de232b28f016b6b857d9203ef6e
Necib <necib@mbp-de-necib.home>
2024-11-10 19:54:40 +0100

    Add moveRight function to align numbers right in each row

--- app.js
+++ app.js
@@ -10,18 +10,39 @@ document.addEventListener('DOMContentLoaded', ()=> {
             square.innerHTML = "0"
             gridDisplay.appendChild(square)
             squares.push(square)
-            console.log(squares)
+            // console.log(squares)
         }
         generate()
         generate()
     }
     createBoard()
 
+    // generate a new number
     function generate() {
         const randomNumber = Math.floor(Math.random() * squares.length)
-        console.log(randomNumber)
         if (squares[randomNumber].innerHTML === "0") {
             squares[randomNumber].innerHTML = "2"
         } else generate()
     }
+
+    // move the number to the right
+    function moveRight() {
+        for(let i=0; i < 16; i++) {
+            if(i % 4 === 0) {
+                let totalOne = squares[i].innerHTML
+                let totalTwo = squares[i+1].innerHTML
+                let totalThree = squares[i+2].innerHTML
+                let totalFour = squares[i+3].innerHTML
+                const row = [parseInt(totalOne),parseInt(totalTwo),parseInt(totalThree),parseInt(totalFour)]
+                // console.log(row)
+                let filteredRow = row.filter(num => num)
+                // console.log(filteredRow)
+                let zeroMissing = 4 - filteredRow.length
+                let zeros = Array(zeroMissing).fill(0)
+                let newRow = zeros.concat(filteredRow)
+                console.log(newRow)
+            }
+        }
+    }
+    moveRight()
 })
